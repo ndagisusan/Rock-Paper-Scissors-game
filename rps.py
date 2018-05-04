f67384c0d2451257aefaffa5c36e30d42e802460
Susan Ndagi <33904019+ndagi-susan@users.noreply.github.com>
2018-05-04 18:49:55 +0300

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,33 @@
+#Rock Paper Scissors
+
+print("Welcome to the Game: rock paper scissors")
+
+player1 = raw_input("Player 1:")    
+while (player1 != "rock" and player1 != "paper" and player1 != "scissors"):
+    print("This is not a valid object selection")
+    print("Please try again!")
+    player1 = raw_input("Player 1:")
+
+player2 = raw_input("Player 2:")
+while (player2 != "rock" and player2 != "paper" and player2 != "scissors"):
+        print("This is not a valid object selection for Player 2")
+        print("Please try again!")
+        player2 = raw_input("Player 2:")
+           
+if player1 == "rock" and player2 == "paper":
+    print("Player 2 wins")
+elif player1 == "rock" and player2 == "scissors":
+    print("Player 1 wins")
+elif player1 == "paper" and player2 == "scissors":
+    print("Player 2 wins")
+elif player1 == "paper" and player2 == "rock":
+    print("Player 1 wins")
+elif player1 == "scissors" and player2 == "rock":
+    print("Player 2 wins")
+elif player1 == "scissors" and player2 == "paper":
+    print("Player 1 wins")
+else:
+    print("Tie! Everybody wins!")
+
+#exit()
+    
